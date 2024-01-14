20d54548d50ae470df5aa385730126bf4f17a072
Fatima Malik <fatimamalik1642@gmail.com>
2024-01-14 22:24:40 +0500

    homepage ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import { Route } from "react-router-dom";
 import ChatPage from './Pages/ChatPage';
+import Homepage from './Pages/HomePage';
+import { extendTheme } from '@chakra-ui/react'
 
+// 2. Add your color mode config
 
 function App() {
+  const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+// 3. extend the theme
+const App= extendTheme({ config })
   return (
     <div className="App">
-      
+      <Route path="/" component={Homepage} exact />
       <Route path = "/chats" component={ChatPage} />
     </div>
   );
